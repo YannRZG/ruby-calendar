653fb2196cd1fe94929f543a1b4737961c47c475
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-01-29 21:26:03 +0100

    update

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
+require 'pry'
 require_relative 'lib/event.rb'
 require_relative 'lib/user.rb'
-# require 'bundler'
-# Bundler.require
 
-my_event = Event.new("2019-01-13 09:00", 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
 
-puts my_event.date
+
+
+binding.pry
+
+
+
+
+
 
 
 
--- lib/event.rb
+++ lib/event.rb
@@ -1,25 +1,47 @@
 require 'pry'
+require 'time'
 
 class Event
     
     attr_accessor :start_date, :duration, :title, :email
-    @@all_email = Array.new
-    @@event = Array.new
+    
 
     def  initialize(date, duration, title, email)
-        @date = date
-        @duration = duration
-        @title = title
-        @email = email
-        @@all_email << email
-        @@event << self
+        @date = Time.parse(date)
+        @duration = duration.to_i
+        @title = title.to_s
+        @email = email.to_a
+            
     end
 
     def postpone_24h
         @date = @date + 24*60*60
     end
 
-    
+    def end_date
+    end_date = @date + @duration 
+    end
+
+    def is_past?
+        Time.now > @date   
+    end
+
+    def is_future?
+        Time.now < @date 
+    end
+
+    def is_soon?
+    end
+
+    def to_s
+        puts "Titre : #{@title}"
+        puts "Date de début : #{@date}"
+        puts "Durée : #{@duration} minutes"
+        puts "Invités : #{@email}"
+    end
+
 
 end
+
+binding.pry
     
